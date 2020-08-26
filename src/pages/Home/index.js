351a8253546aea01ecebae533b4d89618b0333a4
FelipeVieiraa <felipezimba8228@gmail.com>
2020-08-26 15:15:01 -0300

    First commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+ 
+import Shoes from '../../components/Shoes';
+
+export default function Home() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image 
+          source={require("../../assets/banner.png")}
+          style={styles.image}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>TÊNIS</Text>
+          <Text style={[styles.text, { color: '#CECECF' }]}>•</Text>
+          <Text style={[styles.text, { color: '#CECECF' }]}>MASCULINO</Text>
+          <TouchableOpacity 
+            style={styles.buttonFilter}
+          >
+          <MaterialIcons 
+            name="filter-list"
+            size={24}
+            color="#000"
+          />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.line}/>
+
+      <ScrollView>
+        <Text style={styles.text}>LANÇAMENTOS</Text>
+        <View style={styles.shoesContent}> 
+          <Shoes 
+            img={require('../../assets/1.png')}
+            cost="R$280,90"
+            onClick={() => navigation.navigate('Detail')}
+          >
+            Nike Air Max 10
+          </Shoes>
+          <Shoes 
+            img={require('../../assets/1.png')}
+            cost="R$280,90"
+            onClick={() => navigation.navigate('Detail')}
+          >
+            Nike Air Max 10
+          </Shoes>
+        </View>
+
+        <View style={styles.shoesContent}> 
+          <Shoes 
+            img={require('../../assets/1.png')}
+            cost="R$280,90"
+            onClick={() => navigation.navigate('Detail')}
+          >
+            Nike Air Max 10
+          </Shoes>
+          <Shoes 
+            img={require('../../assets/1.png')}
+            cost="R$280,90"
+            onClick={() => navigation.navigate('Detail')}
+          >
+            Nike Air Max 10
+          </Shoes>
+        </View>
+
+        <View style={styles.shoesContent}> 
+          <Shoes 
+            img={require('../../assets/1.png')}
+            cost="R$280,90"
+            onClick={() => navigation.navigate('Detail')}
+          >
+            Nike Air Max 10
+          </Shoes>
+          <Shoes 
+            img={require('../../assets/1.png')}
+            cost="R$280,90"
+            onClick={() => navigation.navigate('Detail')}
+          >
+            Nike Air Max 10
+          </Shoes>
+        </View>
+      </ScrollView>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#FFF',
+  },
+  header: {
+    marginBottom: 8,
+  },
+  image: {
+    width: '100%',
+  },
+  textContainer: {
+    flexDirection: 'row',
+    marginVertical: '5%',
+    marginHorizontal: '5%',
+  },
+  text: {
+    fontFamily: 'Anton_400Regular',
+    fontSize: 26,
+    marginHorizontal: '2%',
+  },
+  buttonFilter: {
+    position: 'absolute',
+    right: 0,
+    alignSelf: 'center',
+  },
+  line: {
+    borderBottomColor: '#D8D8D8',
+    borderBottomWidth: 2,
+  },
+  shoesContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  }
+});
---
+++ src/router.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Feather } from '@expo/vector-icons';
+
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="Detail"
+          component={Detail}
+          options={{ 
+            headerShown: true,
+            headerRight: () => (
+              <TouchableOpacity style={{marginRight: 15}}>
+                <Feather 
+                  name="shopping-cart"
+                  size={24}
+                  color="black"
+                />
+              </TouchableOpacity>
+            )
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
